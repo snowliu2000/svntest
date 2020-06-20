83b54e3a48178405c4db9346b60718fa38cda21a
snow <snowliu58@163.com>
2020-06-20 17:42:55 +0800

    hi

---
+++ src/main/java/com/snow/svntest/SvntestApplication.java
@@ -0,0 +1,13 @@
+package com.snow.svntest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvntestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvntestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snow/svntest/SvntestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snow.svntest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvntestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
